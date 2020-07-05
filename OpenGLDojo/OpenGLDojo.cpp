386e52525102dae8bf7ad1e8f9b8a42665ce9d9e
ambiesoft <ambiesoft.trueff@gmail.com>
2020-07-05 19:26:14 +0900

    2

--- OpenGLDojo/OpenGLDojo.cpp
+++ OpenGLDojo/OpenGLDojo.cpp
@@ -9,15 +9,26 @@
 
 void display()
 {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glRotatef(1, //angle
+        0, 0, 1 // axis, z
+    );
     glutWireTeapot(1);
-    glFlush();
+    // glFlush();
+    glutSwapBuffers();
+}
+void idle()
+{
+    glutPostRedisplay();
 }
 int main(int argc, char* argv[])
 {
     glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE);
     glutInitWindowPosition(640, 0);
     glutInitWindowSize(640, 640);
     glutCreateWindow(APP_TITLE);
     glutDisplayFunc(display);
+    glutIdleFunc(idle);
     glutMainLoop();
 }
