8bc871c9436826bd9744e43bd91c8ede33c9a1a9
ambiesoft <ambiesoft.trueff@gmail.com>
2020-07-06 14:28:06 +0900

    8-fix

---
+++ OpenGLDojo/Rect.cpp
@@ -0,0 +1,29 @@
+#include "Rect.h"
+
+#include "GL/glut.h"
+
+Rect::Rect(const vec2& position, const vec2& size) :
+	m_position(position),
+	m_size(size)
+{
+
+}
+
+void Rect::draw()
+{
+	glRectf(
+		m_position.x, m_position.y,
+		m_position.x + m_size.x, m_position.y + m_size.y
+	);
+}
+bool Rect::intersect(const vec2& point)
+{
+	if (
+		(point.x >= m_position.x) &&
+		(point.x <= (m_position.x + m_size.x)) &&
+		(point.y >= m_position.y) &&
+		(point.y <= (m_position.y + m_size.y))
+		)
+		return true;
+	return false;
+}
---
+++ OpenGLDojo/Rect.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "../glm/glm/glm.hpp"
+
+using namespace glm;
+struct Rect
+{
+	vec2 m_position;
+	vec2 m_size;
+
+	Rect(const vec2& position, const vec2& size);
+	void draw();
+	bool intersect(const vec2& point);
+};
+
